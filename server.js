76fcd79854d5eb25bb6d57ef205cdd0039d9d947
muzyk0 <ru9art@gmail.com>
2022-03-28 15:03:34 +0300

    add .gitignore

--- server.js
+++ server.js
@@ -1,39 +1,40 @@
-const http = require('http')
-const fs = require('fs')
-
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    requestsCount++
-
     switch (req.url) {
-        case '/users': {
-            res.write("Users")
-            res.end()
-            break
-        }
-        case '/portfolio': {
-            res.write('Frontend: React, GraphQL, Redux. Beckend: NodeJS, GraphQL, Express, TypeORM, NestJS, PostgreSQL');
-            res.end()
+        case "/users": {
+            res.write("Users");
+            res.end();
             break;
         }
-        case '/favicon.ico': {
-            fs.readFile('public/favicon.ico', (err, data) => {
-                res.setHeader('Content-Length', data.length);
-                res.setHeader('Content-Type', 'image/x-icon');
+        case "/portfolio": {
+            res.write(
+                "Frontend: React, GraphQL, Redux. Beckend: NodeJS, GraphQL, Express, TypeORM, NestJS, PostgreSQL"
+            );
+            res.end();
+            break;
+        }
+        case "/favicon.ico": {
+            fs.readFile("public/favicon.ico", (err, data) => {
+                res.setHeader("Content-Length", data.length);
+                res.setHeader("Content-Type", "image/x-icon");
                 res.setHeader("Cache-Control", "public, max-age=2592000");
-                res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
+                res.setHeader(
+                    "Expires",
+                    new Date(Date.now() + 2592000000).toUTCString()
+                );
                 res.end(data);
-            })
-        break
+            });
+            break;
         }
 
         default: {
             res.statusCode = 404;
-            res.write('404: not fount')
-            res.end()
+            res.write("404: not fount");
+            res.end();
         }
     }
-})
+});
 
-
-server.listen(3011)
+server.listen(3011);
